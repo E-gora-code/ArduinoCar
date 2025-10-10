f25dd055acebea3951f4da94eafe7bf568db5df9
Egora <e-gora.egor@yandex.ru>
2025-10-10 22:57:33 +0700

    CODED IT!!

--- Code/IR-controll/IR-controll.ino
+++ Code/IR-controll/IR-controll.ino
@@ -14,11 +14,17 @@ int acceleration = 50;
 
 
 IRrecv irrecv(RECV_PIN);
-decode_results  ir_results;
+decode_results  ir_results,_ir_results;
+
+#define IR_blank 0xFFFFFFFF
 
 #define IR_forward 0xFF18E7
 #define IR_stop 0xFF38C7
 #define IR_backwards 0xFF4AB5
+#define IR_right 0xFF5AA5
+#define IR_left 0xFF10EF
+const uint32_t IR_all[]={IR_blank,IR_forward,IR_stop,IR_backwards,IR_right,IR_left};
+int IR_all_size = 6;
 
 bool is_moving = false;
 unsigned long last_stopped = 0;
@@ -38,15 +44,28 @@ void loop() {
   all_run();
   if(ir_results.value == IR_forward){
     stepper_Right.moveTo(stepper_Right.currentPosition()+10000);
+    stepper_Left.moveTo(stepper_Right.currentPosition()-10000);
     is_moving = true;
   }
   else if(ir_results.value == IR_backwards){
     stepper_Right.moveTo(stepper_Right.currentPosition()-10000);
+    stepper_Left.moveTo(stepper_Right.currentPosition()+10000);
+    is_moving = true;
+  }
+  else if(ir_results.value == IR_right){
+    stepper_Right.moveTo(stepper_Right.currentPosition()-10000);
+    stepper_Left.moveTo(stepper_Right.currentPosition()-10000);
+    is_moving = true;
+  }
+  else if(ir_results.value == IR_left){
+    stepper_Right.moveTo(stepper_Right.currentPosition()+10000);
+    stepper_Left.moveTo(stepper_Right.currentPosition()+10000);
     is_moving = true;
   }
   else{
     if(ir_results.value == IR_stop){
       stepper_Right.moveTo(stepper_Right.currentPosition());
+      stepper_Left.moveTo(stepper_Left.currentPosition());
       if(is_moving == true){
         last_stopped = millis();
         is_moving = false;
@@ -85,7 +104,12 @@ void wait_all_step(){
   }
 }
 void update_receve(){
-  if (irrecv.decode(&ir_results))  {   
+  if (irrecv.decode(&_ir_results))  {   
+    if((_ir_results.value==IR_blank)||(!isInCodes(_ir_results.value))){
+      irrecv.resume();
+      return;
+    }
+    ir_results = _ir_results;
     Serial.println(ir_results.value, HEX);
     irrecv.resume(); // Receive  the next value
   }
@@ -100,4 +124,13 @@ void disableMotor(int mot[]) {
   digitalWrite(mot[1], LOW);
   digitalWrite(mot[2], LOW);
   digitalWrite(mot[3], LOW);
+}
+
+bool isInCodes(uint32_t code) {
+    for (int i = 0; i < IR_all_size; i++) {
+        if (IR_all[i] == code) {
+            return true;
+        }
+    }
+    return false;
 }
