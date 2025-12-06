c9b4e58e996c20eea1ac7709089301f9eb57d544
Egora <e-gora.egor@yandex.ru>
2025-12-06 20:57:09 +0700

    Commented it

--- Code/IR_controll_simple_commented/IR_controll_simple_commented.ino
+++ Code/IR_controll_simple_commented/IR_controll_simple_commented.ino
@@ -1,23 +1,23 @@
-#include <AccelStepper.h>
-#include <IRremote.h>
+#include <AccelStepper.h>//Библиотека для управления моторами
+#include <IRremote.h>//Библиотека для приёма ИК сигнала
 int cou = 0;
 
-int stp_R_pins[] = {2,3,4,5};
-int stp_L_pins[] = {6,7,8,9};
+int stp_R_pins[] = {2,3,4,5}; //Пины подключения правого мотора
+int stp_L_pins[] = {6,7,8,9}; //Пины подключения левого мотора
 AccelStepper stepper_Right(AccelStepper::FULL4WIRE, stp_R_pins[0], stp_R_pins[2], stp_R_pins[1], stp_R_pins[3]);
 AccelStepper stepper_Left(AccelStepper::FULL4WIRE, stp_L_pins[0], stp_L_pins[2], stp_L_pins[1], stp_L_pins[3]);
 
-int RECV_PIN = 10;
+int RECV_PIN = 10; // Пин ик приёмника
 
-int speed = 400;
-int acceleration = 50;
+int speed = 400; //Максимальная скорость
+int acceleration = 50; //Ускорение моторов
 
 
 IRrecv irrecv(RECV_PIN);
 decode_results  ir_results,_ir_results;
 
 #define IR_blank 0xFFFFFFFF
-
+//Настройка команд под пульт
 #define IR_forward 0xFF18E7
 #define IR_stop 0xFF38C7
 #define IR_backwards 0xFF4AB5
@@ -42,6 +42,7 @@ void setup() {
 
 void loop() {
   all_run();
+  //Обработка команд
   if(ir_results.value == IR_forward){
     stepper_Right.moveTo(stepper_Right.currentPosition()+10000);
     stepper_Left.moveTo(stepper_Right.currentPosition()-10000);
@@ -72,23 +73,23 @@ void loop() {
       }
     }
   }
-  if((!is_moving)&&(millis()-last_stopped >10000)){
+  if((!is_moving)&&(millis()-last_stopped >10000)){//Робот откулючит питание моторов при бездействии
     disableMotor(stp_R_pins);
     disableMotor(stp_L_pins);
   }
   
 }
-void wait_one_step(AccelStepper stepper){
+void wait_one_step(AccelStepper stepper){//Старая функция ожидания остановки мотора
   while(stepper.distanceToGo()!=0){
     all_run();
   }
 }
-void wait_all_step(){
+void wait_all_step(){//Старая функция ожидания остановки моторов
   while((stepper_Right.distanceToGo()!=0)||(stepper_Left.distanceToGo()!=0)){
     all_run();
   }
 }
-void update_receve(){
+void update_receve(){ //Попытка приёма сигнала
   if (irrecv.decode(&_ir_results))  {   
     if((_ir_results.value==IR_blank)||(!isInCodes(_ir_results.value))){
       irrecv.resume();
@@ -96,22 +97,22 @@ void update_receve(){
     }
     ir_results = _ir_results;
     Serial.println(ir_results.value, HEX);
-    irrecv.resume(); // Receive  the next value
+    irrecv.resume();
   }
 }
-void all_run(){
+void all_run(){ //Обработка всех моторов и приём сигнала
   stepper_Right.run(); 
     stepper_Left.run(); 
     update_receve();
 }
-void disableMotor(int mot[]) {
+void disableMotor(int mot[]) {//Полное выключение питания мотора
   digitalWrite(mot[0], LOW);
   digitalWrite(mot[1], LOW);
   digitalWrite(mot[2], LOW);
   digitalWrite(mot[3], LOW);
 }
 
-bool isInCodes(uint32_t code) {
+bool isInCodes(uint32_t code) {//Функция для проверки на существованииие команды
     for (int i = 0; i < IR_all_size; i++) {
         if (IR_all[i] == code) {
             return true;
